dae3d592ba39ba93af9eb3820fd718529c0dfdf8
Gabriel Netto <biel_netto@hotmail.com>
2020-12-07 21:25:40 -0300

    Added login icons and background image

--- lib/pages/preLogin.dart
+++ lib/pages/preLogin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class PreLoginPage extends StatefulWidget {
@@ -12,26 +13,42 @@ class _PreLoginPageState extends State<PreLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:HexColor('#172957'),
       body: Container(
         alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             // LOGO DO APP
-            Container(width: 300, height: 300, color: Colors.black,),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/preLogin/background.jpg'), 
+                  fit: BoxFit.cover,
+                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop)
+                ),
+              ),
+            ),
             Column(
               children: <Widget>[
-                Container(),
                 RaisedButton.icon(
                   onPressed: (){},
                   label: Text("Entrar com o Google", style: TextStyle(fontSize: 20)),
-                  icon: Icon(Icons.golf_course)
+                  icon: SvgPicture.asset(
+                    'assets/preLogin/google-icon.svg',
+                    height: 20,
+                    width: 20
+                  ),
+                  color: Colors.white
                 ),
                 RaisedButton.icon(
                   onPressed: (){},
-                  label: Text("Entrar com o Google", style: TextStyle(fontSize: 20)),
-                  icon: Icon(Icons.golf_course)
+                  label: Text("Entrar com o Spotify", style: TextStyle(fontSize: 20)),
+                  icon: SvgPicture.asset(
+                    'assets/preLogin/spotify-icon.svg',
+                    height: 20,
+                    width: 20
+                  ),
+                  color: Colors.white
                 ),
                 RaisedButton.icon(
                   onPressed: (){},
