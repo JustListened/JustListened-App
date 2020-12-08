a612d51fdf2459a6a888aa7c5b6675a763c6a3a5
Gabriel Netto <biel_netto@hotmail.com>
2020-12-08 17:57:01 -0300

    Adjusted buttons and background image

--- lib/pages/preLogin.dart
+++ lib/pages/preLogin.dart
@@ -16,20 +16,33 @@ class _PreLoginPageState extends State<PreLoginPage> {
       body: Container(
         alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             // LOGO DO APP
             Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/preLogin/background.jpg'), 
+                  image: AssetImage('assets/preLogin/backgroundImage.jpg'), 
                   fit: BoxFit.cover,
-                  colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop)
+                  // colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop)
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
                 ),
               ),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height/1.3,
+              margin: EdgeInsets.only(bottom: 10),
             ),
             Column(
               children: <Widget>[
+                RaisedButton(
+                  onPressed: (){},
+                  child: Text("Entrar com o Aplicativo", style: TextStyle(fontSize: 20)),
+                  // icon: Icon(Icons.golf_course),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                ),
                 RaisedButton.icon(
                   onPressed: (){},
                   label: Text("Entrar com o Google", style: TextStyle(fontSize: 20)),
@@ -38,23 +51,20 @@ class _PreLoginPageState extends State<PreLoginPage> {
                     height: 20,
                     width: 20
                   ),
-                  color: Colors.white
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                 ),
                 RaisedButton.icon(
                   onPressed: (){},
-                  label: Text("Entrar com o Spotify", style: TextStyle(fontSize: 20)),
+                  label: Text("Entrar com o Spotify", style: TextStyle(fontSize: 20, color: Colors.green)),
                   icon: SvgPicture.asset(
                     'assets/preLogin/spotify-icon.svg',
                     height: 20,
                     width: 20
                   ),
-                  color: Colors.white
+                  color: Colors.black,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                 ),
-                RaisedButton.icon(
-                  onPressed: (){},
-                  label: Text("Entrar com o Google", style: TextStyle(fontSize: 20)),
-                  icon: Icon(Icons.golf_course)
-                )
               ],
             ),
           ]
